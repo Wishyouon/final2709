8850a540dc76bfee7589fe2d166787cdedece5ac
Wishyouon <126260152+Wishyouon@users.noreply.github.com>
2023-08-16 20:23:54 +0530

    run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/run-python', methods=['GET'])
 def run_python():
-    output = subprocess.check_output(['python', 'script.py'])
+    output = subprocess.check_output(['python', 'app.py'])
     return jsonify(output=output.decode('utf-8'))
 
 
